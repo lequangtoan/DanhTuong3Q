12d50d3eca950be62a9f09196099b9f30aace34a
Toan Le <toanlee96@gmail.com>
2024-12-18 10:21:52 +0700

    0.0.9.5.5

--- src/components/home-section/index.js
+++ src/components/home-section/index.js
@@ -1,7 +1,7 @@
 import Welcome from "./welcome/Welcome";
 import News from "./news/News";
 import Person from "./person/Person";
-import Comic from "./comic/Comic";
+import Comic from "./Comic/Comic";
 import Feature from "./feature/Feature";
 
 export { Welcome, News, Person, Comic, Feature };
