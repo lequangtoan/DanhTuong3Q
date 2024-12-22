fe1df7ecc9cf8df94e40303734425dd1d4bc3190
Toan Le <toanlee96@gmail.com>
2024-12-23 00:28:41 +0700

    0.0.9.8.4

--- src/components/drawer/Drawer.jsx
+++ src/components/drawer/Drawer.jsx
@@ -13,39 +13,7 @@ import {
 
 const Drawer = () => {
   return (
-    // <div className="drawer">
-    //   <div className="drawer__info__app">
-    //     <a className="drawer__icongame" href="##">
-    //       <img src={icongame} alt="" />
-    //     </a>
-
-    //     <a className="drawer__appstore" href="http://">
-    //       <img src={appstore} alt="" />
-    //     </a>
-    //     <a className="drawer__appstore" href="http://">
-    //       <img src={chplay} alt="" />
-    //     </a>
-
-    //     <a className="drawer__gialappc" href="http://">
-    //       <img src={gialappccolum} alt="" />
-    //     </a>
-    //     <div className="drawer__info__app__pay">
-    //       <a className="drawer__napthe" href="http://">
-    //         <img src={napthe} alt="" />
-    //       </a>
-    //     </div>
-
-    //     <div className="drawer__info__Group">
-    //       <a className="drawer__congdong" href="http://">
-    //         <img src={congdong} alt="" />
-    //       </a>
-    //       <a className="drawer__facebook" href="http://">
-    //         <img src={facebook} alt="" />
-    //       </a>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className="slide">
+    <div className="slide" id="slide">
       {/* <img className="slide-btn" src="" alt="close" /> */}
       <div className="slide-content">
         <a href="##" className="slide-content-btn">
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -23,6 +23,54 @@ const swiperOptions = {
 };
 
 const Home = () => {
+  const [activeSection, setActiveSection] = useState(null);
+
+  useEffect(() => {
+    const sectionswelcome = document.querySelectorAll(".welcome");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const sliderwelcome = document.getElementById("slide");
+          if (entry.isIntersecting) {
+            sliderwelcome?.classList?.remove("show");
+          } else {
+            sliderwelcome?.classList?.add("show");
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+    sectionswelcome.forEach((section) => observer.observe(section));
+    return () => {
+      sectionswelcome.forEach((section) => observer.unobserve(section));
+    };
+  }, []);
+
+  useEffect(() => {
+    const sectionsfeature = document.querySelectorAll(".feature");
+    console.log("11", sectionsfeature)
+    const scrolled = new IntersectionObserver(
+      (entries) => {
+        // entries.forEach((entry) => {
+          const scrolled = document.getElementById("scroll");
+          console.log("111", scrolled)
+          entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            scrolled?.classList?.remove("hidden");
+          } else {
+            scrolled?.classList?.add("hidden");
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+    sectionsfeature.forEach((section) => scrolled.observe(section));
+    return () => {
+      sectionsfeature.forEach((section) => scrolled.unobserve(section));
+    };
+  }, []);
+
+
   return (
     <>
       <Swiper {...swiperOptions}>
@@ -42,7 +90,7 @@ const Home = () => {
           {({ isActive }) => <Feature isActive={isActive} />}
         </SwiperSlide>
       </Swiper>
-      <div className="scrolled">
+      <div className="scrolled" id="scroll">
         <span>
           <img src={scrolling} alt="" />
         </span>
